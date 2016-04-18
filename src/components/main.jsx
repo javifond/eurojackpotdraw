c9a3bfa411f86db72e96069a742d115784574637
Javier Betancourth <javifond@gmail.com>
2016-04-18 23:00:09 +0200

    feature: add routing and first visible content

---
+++ src/components/main.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+module.exports = React.createClass({
+  render: function() {
+    return <div>
+      <h1>EuroJackpot Results & Winning Numbers</h1>
+    </div>
+  }
+});
